cba7d89a238113d04db681ab6a0ec7400f6c7b35
Dede Ari <dedeari006@gmail.com>
2025-06-14 19:53:03 +0700

    menambahkan

---
+++ string.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Dede Ari")
+	fmt.Println("Dede Ari1")
+	fmt.Println("Dede Ari2")
+
+	fmt.Println(len("Dede Ari"))
+	fmt.Println("Dede Ari1"[0])
+	fmt.Println("Dede Ari2"[1])
+}
